c23ed31cd7cb7c6d2e62fa8d184ce543c5a2ebaf
Preet Vishwakarma <159623026+preet-99@users.noreply.github.com>
2026-09-08 16:07:40 +0530

    Fix indentation and update credit card options

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ credit_score = st.number_input('Credit Score')
 estimated_salary = st.number_input('Esmtimated Salary')
 tenure = st.slider('Tenure', 0, 10)
 num_of_products = st.slider('Number of Products',1, 4)
-has_cr_card = st.selectbox("Has Credit Card",[1, 4])
+has_cr_card = st.selectbox("Has Credit Card",[1, 2, 3, 4])
 is_active_member = st.selectbox('Is Active Member', [0,1])
 
 ## Prepare the input data 
